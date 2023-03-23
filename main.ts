0e4f9ddf331766511eb3213015c29776d2d78c5e
ShaanVermaP1N <128705584+ShaanVermaP1N@users.noreply.github.com>
2023-03-23 20:23:00 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    num1 = 6
+    num2 = 3
+    basic.showNumber(num1)
+})
+input.onButtonPressed(Button.B, function () {
+    result = num1 ** num2
+    basic.showNumber(result)
+})
+let num2 = 0
+let num1 = 0
+let result = 0
+result = 0
+num1 = 0
+num2 = 0
+basic.showNumber(result)
